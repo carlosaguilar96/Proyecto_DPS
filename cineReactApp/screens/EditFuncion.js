334fc580591a2edd983d6d6bab5f10932bc47927
MonicaLRM <rivera6577@gmail.com>
2024-10-03 15:27:28 -0600

    commit edicion y perfil

--- cineReactApp/screens/EditFuncion.js
+++ cineReactApp/screens/EditFuncion.js
@@ -63,6 +63,7 @@ const ModificarFuncion = () => {
 
   const CambiarEstado = () => {
     setShowModalEstado(false); // Mostrar el modal
+    //AQUI DEBERIA CAMBIARSE EL ESTADO, en id esta el id de funcion
     console.log(id);
   };
 
@@ -80,7 +81,9 @@ const ModificarFuncion = () => {
             <Text style={estilos.textoGrande}>Precio Niños: ${item.precioNino}</Text>
             <Text style={estilos.textoGrande}>Precio Adultos: ${item.precioAdulto}</Text>
             <Text style={estilos.textoGrande}>Precio 3ra Edad: ${item.precioTE}</Text>
-            <View style={estilos.contenedorBotones}>
+            
+          </View>
+          <View style={estilos.contenedorBotones}>
               <TouchableOpacity style={estilos.botonIcono} onPress={() => handleEditPress(item.id)}>
                 <FontAwesome name="edit" size={30} color="white" />
               </TouchableOpacity>
@@ -94,7 +97,6 @@ const ModificarFuncion = () => {
             </TouchableOpacity>
             )}
             </View>
-          </View>
         </View>
       </View>
     );
@@ -124,64 +126,74 @@ const ModificarFuncion = () => {
 
       {/* Modal para seleccionar fecha y hora */}
       <Modal
-        visible={showModal}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setShowModal(false)}
-      >
-        <View style={estilos.modalContainer}>
-          <View style={estilos.modalContent}>
-            <Text style={estilos.modalTitle}>Modificar Función</Text>
-            <Text style={estilos.textoGrande}>Ingrese la nueva fecha:</Text>
-            {/* Botón para seleccionar la fecha */}
-            <Button title="Seleccionar Fecha" onPress={() => setShowDatePicker(true)} />
-            {showDatePicker && (
-              <DateTimePicker
-                value={date}
-                mode="date"
-                display="default"
-                onChange={onDateChange}
-              />
-            )}
+      visible={showModal}
+      animationType="slide"
+      transparent={true}
+      onRequestClose={() => setShowModal(false)}
+    >
+      <View style={estilos.modalContainer}>
+        <View style={estilos.modalContent}>
+          <Text style={estilos.modalTitle}>Modificar Función</Text>
+          
+          <Text style={estilos.textoGrande}>Ingrese la nueva fecha:</Text>
+          <TouchableOpacity style={estilos.botonSeleccionar} onPress={() => setShowDatePicker(true)}>
+            <Text style={estilos.botonTexto}>Seleccionar Fecha</Text>
+          </TouchableOpacity>
+          {showDatePicker && (
+            <DateTimePicker
+              value={date}
+              mode="date"
+              display="default"
+              onChange={onDateChange}
+            />
+          )}
 
-            {/* Botón para seleccionar la hora */}
-            <Text style={estilos.textoGrande}>Ingrese la nueva hora:</Text>
-            <Button title="Seleccionar Hora" onPress={() => setShowTimePicker(true)} />
-            {showTimePicker && (
-              <DateTimePicker
-                value={date}
-                mode="time"
-                is24Hour={false}
-                display="default"
-                onChange={onTimeChange}
-              />
-            )}
+          <Text style={estilos.textoGrande}>Ingrese la nueva hora:</Text>
+          <TouchableOpacity style={estilos.botonSeleccionar} onPress={() => setShowTimePicker(true)}>
+            <Text style={estilos.botonTexto}>Seleccionar Hora</Text>
+          </TouchableOpacity>
+          {showTimePicker && (
+            <DateTimePicker
+              value={date}
+              mode="time"
+              is24Hour={false}
+              display="default"
+              onChange={onTimeChange}
+            />
+          )}
 
-            {/* Botones para aceptar o cancelar */}
-            <View style={estilos.modalButtons}>
-              <Button title="Guardar" onPress={() => editFuncion()} />
-              <Button title="Cancelar" onPress={() => setShowModal(false)} />
-            </View>
+          <View style={estilos.modalButtons}>
+            <TouchableOpacity style={estilos.botonGuardar} onPress={editFuncion}>
+              <Text style={estilos.botonTexto}>Guardar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={estilos.botonCancelar} onPress={() => setShowModal(false)}>
+              <Text style={estilos.botonTexto}>Cancelar</Text>
+            </TouchableOpacity>
           </View>
         </View>
-      </Modal>
+      </View>
+    </Modal>
        {/* Modal para cambio de estado*/}
        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={ShowModalEstado}
-          onRequestClose={() => setShowModalEstado(false)}
-        >
-          <View style={estilos.modalContainer}>
-            <View style={estilos.modalContent}>
-              <Text style={estilos.modalText}>Seguro que deseas cambiar el estado de la pelicula?</Text>
-              <View style={estilos.buttonContainer}>
-                <Button title="Cancelar" onPress={() => setShowModalEstado(false)} />
-                <Button title="Aceptar" onPress={() => CambiarEstado()} />
-              </View>
-            </View>
+      animationType="slide"
+      transparent={true}
+      visible={ShowModalEstado}
+      onRequestClose={() => setShowModalEstado(false)}
+    >
+      <View style={estilos.modalContainer}>
+        <View style={estilos.modalContent}>
+          <Text style={estilos.modalText}>¿Seguro que deseas cambiar el estado de la película?</Text>
+          <View style={estilos.modalButtons}>
+            <TouchableOpacity style={estilos.botonCancelar} onPress={() => setShowModalEstado(false)}>
+              <Text style={estilos.botonTexto}>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={estilos.botonAceptar} onPress={() => CambiarEstado()}>
+              <Text style={estilos.botonTexto}>Aceptar</Text>
+            </TouchableOpacity>
           </View>
-        </Modal>
+        </View>
+      </View>
+    </Modal>
     </ScrollView>
   );
 };
@@ -192,7 +204,10 @@ const estilos = StyleSheet.create({
     backgroundColor: '#fff',
   },
   tarjeta: {
+    flexDirection: 'row',
     padding: 15,
+    paddingBottom: 5,
+    paddingTop: 0,
     margin: 15,
     backgroundColor: '#f8f8f8',
     borderRadius: 10,
@@ -215,9 +230,11 @@ const estilos = StyleSheet.create({
     marginBottom: 5,
   },
   contenedorBotones: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginTop: 20,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 90,
+    gap: 50,
   },
   botonIcono: {
     backgroundColor: '#8B0000',
@@ -258,6 +275,48 @@ const estilos = StyleSheet.create({
     justifyContent: 'space-around',
     width: '100%',
     paddingTop:10,
+  },
+  modalText: {
+    fontSize: 18,
+ 
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  botonGuardar: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+    marginRight: 5,
+  },
+  botonCancelar: {
+    backgroundColor: '#f44336',
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+    marginLeft: 5,
+  },
+  botonAceptar: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+    marginLeft: 5,
+  },
+  botonTexto: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  botonSeleccionar: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
