5beeee750e5eaf2f0792efc92d07d749a6a9ca84
SomeRandomCatholic <christoamilcruz@gmail.com>
2024-10-04 16:46:39 -0600

    Add sala

--- cineReactApp/screens/AddSala.js
+++ cineReactApp/screens/AddSala.js
@@ -1,19 +1,88 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Alert, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import axios from 'axios';
+import { API_URL } from '@env';
 
-const AñadirSala = () => {
-  const [sucursal, setSucursal] = useState('');
-  const [tipoSala, setTipoSala] = useState('');
+const AñadirSala = ({navigation}) => {
+  const [sucursal, setSucursal] = useState(-1);
+  const [tipoSala, setTipoSala] = useState(-1);
+  const [sucursales, setSucursales] = useState([]);
 
-  const manejarAñadirSala = () => {
+  const validar = () => {
 
-    console.log({ sucursal, tipoSala });
+    if(sucursal == -1){
+      Alert.alert("Mensaje","Debe seleccionar una sucursal");
+      return;
+    }
+    if(tipoSala == -1){
+      Alert.alert("Mensaje","Debe seleccionar un tipo de sala");
+      return;
+    }
+
+    guardarSala();
+
   };
+
+  
+  const guardarSala = async () =>{
+    try {
+      const response = await axios.post(`${API_URL}/api/salas/crearSala`, {
+          sucursal: sucursal
+      });
+      Alert.alert('Registro exitoso', 'Sala creada correctamente');
+      setSucursal(-1);
+      setTipoSala(-1);
+      navigation.navigate("Menu Admin");
+
+  } catch (error) {
+      if (error.response) {
+          const errores = error.response.data.errors;
+          let mensaje = "";
+          for (const campo in errores) {
+              mensaje += `Error en ${campo}: ${errores[campo].join(', ')}`;
+          }
+          setMssgError(mensaje);
+          return;
+      } else if (error.request) {
+          Alert.alert('Error', 'No hubo respuesta del servidor');
+          return;
+      } else {
+          Alert.alert('Error', 'Error al hacer la solicitud ' + error);
+          return;
+      }
+  }
+  }
+
+  const obtenerSucursales = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/api/sucursales/index`);
+
+      if (response.data.sucursales.length != 0) {
+
+        setSucursales(response.data.sucursales);
+
+      }
+
+    } catch (error) {
+      if (error.request) {
+        Alert.alert('Error', 'No hubo respuesta del servidor ');
+        console.log(error);
+        return;
+      } else {
+        Alert.alert('Error', 'Error al hacer la solicitud');
+        return;
+      }
+    }
+  }
+
+  useEffect(() => {
+    obtenerSucursales();
+  });
 
   return (
     <ScrollView style={estilos.contenedor}>
-      
+
       <View style={estilos.formulario}>
         <Text>Sucursal:</Text>
         <Picker
@@ -21,11 +90,11 @@ const AñadirSala = () => {
           style={estilos.entrada}
           onValueChange={(itemValue) => setSucursal(itemValue)}
         >
-          <Picker.Item label="Elija la sucursal en la que añadirá la sala" value="" />
-          <Picker.Item label="Sucursal 1" value="sucursal1" />
-          <Picker.Item label="Sucursal 2" value="sucursal2" />
+          <Picker.Item label="Elija la sucursal en la que añadirá la sala" value={-1} />
+          {sucursales.map((item) => (
+            <Picker.Item label={item.sucursal} value={item.codSucursal} key={() => item.codSucursal} />
+          ))}
         </Picker>
-
 
         <Text>Tipo de Sala:</Text>
         <Picker
@@ -37,7 +106,7 @@ const AñadirSala = () => {
           <Picker.Item label="Estandar" value="Estandar" />
         </Picker>
 
-        <TouchableOpacity style={estilos.botonAñadir} onPress={manejarAñadirSala}>
+        <TouchableOpacity style={estilos.botonAñadir} onPress={validar}>
           <Text style={estilos.textoBotonAñadir}>Añadir Sala</Text>
         </TouchableOpacity>
       </View>
