ba119996c5cbced40c5a9e75415beda68fc5fc62
MonicaLRM <rivera6577@gmail.com>
2024-10-04 19:52:34 -0600

    arreglo en boletos

--- cineReactApp/screens/Boletos.js
+++ cineReactApp/screens/Boletos.js
@@ -46,6 +46,14 @@ export default function Boletos() {
     setabueB(0);
   }
 
+  useFocusEffect(
+    React.useCallback(() => {
+      setchildB(0);
+      setabueB(0);
+      setadultoB(0);
+    }, [])
+  );
+
   const handleBack = () => {
     navigation.goBack();
   };
